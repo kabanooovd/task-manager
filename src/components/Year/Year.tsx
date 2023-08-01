077ee7a960928cc654a717f1f09f54ea11052f7a
Кабанов Дмитрий Алексеевич <d.a.kabanov@samsmu.ru>
2023-08-01 22:39:04 +0300

    finished years ui

--- src/components/Year/Year.tsx
+++ src/components/Year/Year.tsx
@@ -1,7 +1,9 @@
+import moment from 'moment'
 import { MONTHS_MAPPER } from '../../config'
 import { onGetDaysInMonth, onGetOtherMonthDays } from '../../utils'
 import { ModeWrapper } from '../ModeWrapper/ModeWrapper'
 import s from './Year.module.scss'
+import classNames from 'classnames'
 
 export const Year: React.FC<{
     currentYear: number
@@ -10,7 +12,7 @@ export const Year: React.FC<{
     const monthsInYear = Array(12)
         .fill(null)
         .map((_, idx) => idx)
-
+    const _format = 'DD-MM-YYYY'
     return (
         <ModeWrapper>
             <div className={s.container}>
@@ -25,8 +27,6 @@ export const Year: React.FC<{
                             initWeekDayInMonth,
                             'prev'
                         )
-
-                        console.log('==> ', month)
                         return (
                             <div key={month} className={s.calendar__wrapper}>
                                 <div className={s.calendar}>
@@ -34,14 +34,23 @@ export const Year: React.FC<{
                                         <span>{MONTHS_MAPPER[month]}</span>
                                     </div>
                                     <div className={s.calendar_days__wrapper}>
-                                        {/* fake days in the month */}
                                         {prevDaysList.map((_, idx) => (
                                             <div key={idx} />
                                         ))}
                                         {daysList.map((day) => {
+                                            const onSetDayStyle = () => {
+                                                switch (true) {
+                                                    case day.format(_format) === moment(new Date()).format(_format):
+                                                        return classNames(s.day, s.dayTd)
+                                                    case day.day() === 0 || day.day() === 6:
+                                                        return classNames(s.day, s.dayWe)
+                                                    default:
+                                                        return s.day
+                                                }
+                                            }
                                             return (
                                                 <div key={day.date()} className={s.day_grid__wrapper}>
-                                                    <div className={s.day}>{day.date()}</div>
+                                                    <div className={onSetDayStyle()}>{day.date()}</div>
                                                 </div>
                                             )
                                         })}
